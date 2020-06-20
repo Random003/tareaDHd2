63a15b581e6e1beb68548e635b83004e5705a11e
PABLO <>
2020-06-20 20:55:39 -0300

    guardado cena b

--- Ancestro/app-notas/app.js
+++ Ancestro/app-notas/app.js
@@ -2,6 +2,18 @@ const fs = require ("fs");
 const tareasJson = fs. readFileSync ("./tareas.json", "utf-8");
 const tareas = JSON.parse (tareasJson);
 
-tareas.forEach(element => {
-    console.log(element.titulo);
-});
+
+//let parametros = process.argv;
+let accion = process.argv[2];
+
+
+switch (accion) {
+    case "todas":
+        tareas.forEach(element => {
+           console.log(element.titulo);
+        });
+        break;
+      default:
+          console.log();
+          console.log("No entiendo esa acción");
+}

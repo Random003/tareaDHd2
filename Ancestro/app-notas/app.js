93447871fd6040bf60f87bd718fc7133b1df03be
Random003 <mealla.jp@gmail.com>
2020-06-22 21:26:02 -0300

    mas chalk

--- Ancestro/app-notas/app.js
+++ Ancestro/app-notas/app.js
@@ -1,5 +1,5 @@
 const tareas = require ("./tareas");
-
+const chalk = require ("chalk");
 
 //let parametros = process.argv;  argv tiene los argumentos el primero es el ejecutable de node y el segundo el app.js
 //el [2] es para que tome el ejecutable app.js y le agregue el pad (osea lo que escribo despues de node app.js, osea node app.js pendientes)
@@ -9,79 +9,30 @@ let parametros = process.argv.slice(3);
 
 switch (accion) {
     case undefined:
+
+
     case "todas":
-         tareas.todas()
+         tareas.todas();
         break;
+
+
     case "pendientes":
-        
+        tareas.pendientes();
         break; 
 
-        // Microdesafío
-        // 1. Atajar el caso de 'crear'
-        // 2. Hacer un console.log() de el titulo y de la descripción
-        // 3. Crear un objeto literal a partir de lo que envía el usuario
-        // {
-        //     "titulo": "una nueva",
-        //     "descripcion": "con su descripción",
-        //     "estado": "pendiente"
-        //    }
-
+        
     case "crear" :
-      let tareaNueva = {
-          titulo: parametros [0],
-          descripcion: parametros [1],
-          estado: "pendiente"
-      }  
-       
-       tareas.push(tareaNueva);
-
-       escribirArchivoJSON(tareas);
-       
-      console.log("Tarea creada con éxito!");
-      
-
+        tareas.crear(parametros[0], parametros[1]) 
     break 
     
 
-
-    // Micro desafío
-    // 1. Atajar el caso de 'completar'
-    // 2. Vamos a recorrer el array de tareas
-    //  - usar map()
-    //  - usar un if()
-    // 3. Vamos a modificar la tarea que corresponda
-    // 4. Vamos a guardar los cambios
-
-
-    // Micro desafío
-    // 1. Atajar el caso de 'borrar'
-    // 2. Vamos a recorrer el array de tareas
-    //  - usar filter()
-    // 3. Vamos a filtrar la tarea que corresponda
-    // 4. Vamos a guardar los cambios
-
-
     case "borrar":
-        let tareasActualizadas = tareas.filter (
-            function(tarea) {
-                return parametros[0] !== tarea.titulo;
-            }
-        );
-
-            
-         if (tareas.length !== tareasActualizadas.length) {
-                escribirArchivoJSON(tareasActualizadas);
-                console.log("Tarea Borrada Viejita!!");
-         } else {
-                console.log("No esta la tarea, pero si el potrillo!");
-            }
-            
-
+        tareas.borrar(parametros[0])
         break
+
 
       //default ataja cualquier otro caso (similar al "else" que usamos en el "if")
     default:
           console.log();
           console.log("No entiendo esa acción");         
 }
-
--- Ancestro/app-notas/tareas.js
+++ Ancestro/app-notas/tareas.js
@@ -1,4 +1,5 @@
 const fs = require ("fs");
+const chalk = require("chalk");
 
 function leerArchivoJSON () {
     const tareasJson = fs. readFileSync ("./tareas.json", "utf-8");
@@ -24,8 +25,21 @@ function todas() {
     });
 }
 
+
+
+// Micro desafío
+// 1. Crear función listar
+// Toma como parámetro opcional el estado (pendientes, terminadas, etc)
+// - Si llega el estado lista solo las de ese estado
+// - Si no llega el estado, lista todas
+
+// function listar ()
+
+
+
+
 function pendientes (){
-    let tareas = leerArchivoJSON
+    let tareas = leerArchivoJSON();
 
     let tareasFiltradas = tareas.filter(elem => elem.estado == "pendiente"); 
     tareasFiltradas.forEach(element => {
@@ -34,10 +48,85 @@ function pendientes (){
 }
 
 
+        // Microdesafío
+        // 1. Atajar el caso de 'crear'
+        // 2. Hacer un console.log() de el titulo y de la descripción
+        // 3. Crear un objeto literal a partir de lo que envía el usuario
+        // {
+        //     "titulo": "una nueva",
+        //     "descripcion": "con su descripción",
+        //     "estado": "pendiente"
+        //    }
 
+
+function crear(titulo = "", descripcion = "", estado = "pendiente") {
+
+    if (titulo.length > 5) {
+
+        let tareas = leerArchivoJSON();
+
+        let tareaNueva = {
+            titulo: titulo,
+            decripcion: descripcion,
+            estado: estado
+        }
+        
+        tareas.push(tareaNueva);
+        
+        escribirArchivoJSON(tareas);
+        
+        console.log(chalk.inverse.green("Tarea creada con éxito!"))
+
+    } else {
+        console.log(chalk.inverse.redBright("Tenes que ingresar un título y debe tener minimo 5 caracteres"));
+    }
+
+}
+  
+
+
+    // Micro desafío
+    // 1. Atajar el caso de 'completar'
+    // 2. Vamos a recorrer el array de tareas
+    //  - usar map()
+    //  - usar un if()
+    // 3. Vamos a modificar la tarea que corresponda
+    // 4. Vamos a guardar los cambios
+
+    //  function completar ()
+
+
+
+    // Micro desafío
+    // 1. Atajar el caso de 'borrar'
+    // 2. Vamos a recorrer el array de tareas
+    //  - usar filter()
+    // 3. Vamos a filtrar la tarea que corresponda
+    // 4. Vamos a guardar los cambios
+
+    function borrar(titulo) {
+        let tareas = leerArchivoJSON();
+
+        let tareasActualizadas = tareas.filter (
+            function(tarea) {
+                return titulo !== tarea.titulo;
+            }
+        );
+
+            
+         if (tareas.length !== tareasActualizadas.length) {
+                escribirArchivoJSON(tareasActualizadas);
+                console.log(chalk.inverse.greenBright("Tarea Borrada Viejita!!"));
+         } else {
+                console.log(chalk.inverse.red("No esta la tarea, pero si el potrillo!"));
+            }
+    }
+        
 
 
 module.exports = {
-    todas : todas,
-    pendientes : pendientes
+    todas, // todas : todas,
+    pendientes, // pendientes : pendientes
+    crear,
+    borrar,
 }
